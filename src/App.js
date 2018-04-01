034f0dad7c516aacb49dc7646ba5fa9606aa507c
Valerio Innocenti Sedili <valerio.innocenti.ext@yoox.com>
2018-04-01 23:04:16 +0200

    install react libraries

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Container, Box } from "bloomer";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Container>
+        <Box>Hello World!</Box>
+      </Container>
     );
   }
 }
